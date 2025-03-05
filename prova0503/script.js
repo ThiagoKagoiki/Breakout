0fd270f35aca311f4cd45441532c7db518745b1b
THIAGO YUKIO DA COSTA KAGOIKI <thiago_kagoiki@estudante.sesisenai.org.br>
2025-03-05 16:48:02 -0300

    adicionado obejeto com paredes

--- prova0503/script.js
+++ prova0503/script.js
@@ -1,13 +1,15 @@
 const canvas = document.getElementById('breakout');
 const ctx = canvas.getContext('2d');
+var gameover = false
 
 class Entidade {
 
-    constructor(x, y, largura, altura) {
+    constructor(x, y, largura, altura, velocidadex) {
         this.x = x;
         this.y = y;
         this.largura = largura;
         this.altura = altura;
+        this.velocidadex = velocidadex;
     }
 
     desenhar(ctx, cor) {
@@ -22,31 +24,75 @@ class Entidade {
 
 class Plano extends Entidade {
 
-    #velocidadex;
-
-    constructor(x, y, largura, altura) {
-        super(x, y, largura, altura);
-        this.#velocidadex = 0;
+    constructor(x, y, largura, altura, velocidadex) {
+        super(x, y, largura, altura, velocidadex);
+        this.velocidadex = 0;
     }
 
     movimentacaoEsq() {
-        this.#velocidadex = -5;
+        this.velocidadex = -5;
     }
 
     movimentacaoDir() {
-        this.#velocidadex = 5;
+        this.velocidadex = 5;
     }
 
     parede() {
-        if(this.x <= 0 && this.#velocidadex < 0) {
-            this.#velocidadex = 0;
-        }else if((this.x + this.largura) >= canvas.width && this.#velocidadex > 0) {
-            this.#velocidadex = 0;
+        if(this.x <= 0 && this.velocidadex < 0) {
+            this.velocidadex = 0;
+        }else if((this.x + this.largura) >= canvas.width && this.velocidadex > 0) {
+            this.velocidadex = 0;
         }
     }
     
     atualizar() {
-        this.x += this.#velocidadex;
+        this.x += this.velocidadex;
+    }
+}
+
+class Bola extends Entidade{
+
+    #velocidadey;
+
+    constructor(x, y, largura, altura, velocidadex) {
+        super(x, y, largura, altura, velocidadex);
+        this.velocidadex = -5;
+        this.#velocidadey = 5;
+        
+    }
+
+    final(){
+        if(this.y + this.altura >= canvas.height) {
+            this.#velocidadey = 0
+            this.velocidadex = 0
+            gameover = true
+        }
+    }
+
+    colisaoRaquete(){
+        if(this.y + this.altura >= plano.altura + plano.y && this.x + this.largura >= plano.x && this.x <= plano.largura + plano.x && gameover == false){
+            this.#velocidadey = - 5
+            this.velocidadex = -5
+        }
+    }
+    
+    paredeLaterais(){
+        if(this.x >= canvas.width){
+            this.velocidadex = -5
+        }else if(this.x <= 0){
+            this.velocidadex = 5
+        }
+    }
+
+    teto(){
+        if(this.y <= 0){
+            this.#velocidadey = 5
+        }
+    }
+
+    atualizar() {
+        this.x += this.velocidadex;
+        this.y += this.#velocidadey;
     }
 }
 
@@ -59,13 +105,22 @@ document.addEventListener('keypress', (e) => {
 });
 
 const plano = new Plano(canvas.width/2.4, 360, 60, 7)
+const bola = new Bola(190, 337, 10, 10)
 
 function loop(){
+
+    bola.teto()
+    bola.paredeLaterais()
+    bola.final()
+    bola.colisaoRaquete()
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    bola.desenhar(ctx, 'red')
+    bola.atualizar()
     plano.parede()
     plano.atualizar();
     plano.desenhar(ctx, 'white')
     requestAnimationFrame(loop);
+    
 }
 
 loop()
